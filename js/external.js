3beabdd55947690ddfc778a6c0d78e6acd174e52
Carlos DeAnda <reynaldo.deanda@gmail.com>
2022-10-12 18:03:00 -0500

    pushing external.js exercise

---
+++ js/external.js
@@ -0,0 +1,8 @@
+const x = "Hello from external JavaScript";
+console.log(x);
+
+alert("Welcome to My Website!");
+
+const userInput = prompt("What is your favorite number?");
+console.log('The user entered: ' + userInput);
+
