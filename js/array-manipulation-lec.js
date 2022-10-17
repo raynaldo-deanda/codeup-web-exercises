d18217ae14acd99b6bd536c3f42cc26582bb00e8
Carlos DeAnda <reynaldo.deanda@gmail.com>
2022-10-17 13:51:10 -0500

    pushing my exercises

---
+++ js/array-manipulation-lec.js
@@ -0,0 +1,23 @@
+//let nums = [1,2,3,4,5,6,7,8,9,10]
+// nums.push(13)
+//nums.push(11,12,13)
+//nums.unshift(-2,-1,0)
+
+//console.log(nums)
+
+//nums.pop();
+//nums.shift();
+//nums.shift();
+//console.log(nums);
+//console.log(nums)
+
+let nameArr = ["jim","joe","jim","john","jim","bob"];
+
+//console.log(nameArr.indexOf("john"));
+//console.log(nameArr.indexOf("jim"));
+//console.log(nameArr.lastIndexOf("jim"));
+
+//let nameString = "jim, joe, jim, john, jim, bob";
+//console.log(nameString.split(",", 2))
+
+console.log(nameArr.join(", "))
